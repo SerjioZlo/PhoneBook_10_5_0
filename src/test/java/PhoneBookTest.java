b683a45be856d9b5dfdc9a418aa04595280261c0
serjzlo <serioga2003@yandex.ru>
2023-07-31 15:09:55 +0700

    Stub and tests for findByName

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -12,10 +12,13 @@ public class PhoneBookTest {
 
         result = phoneBook.add("Jane", "67890");
         Assert.assertEquals(2, result);
+
+        result = phoneBook.add("John", "54321");
+        Assert.assertEquals(-1, result);
     }
 
     @Test
-    public void testFindByNumber(){
+    public void testFindByNumber() {
         phoneBook.add("John", "12345");
 
         String result = phoneBook.findByNumber("12345");
@@ -27,6 +30,17 @@ public class PhoneBookTest {
 
         result = phoneBook.findByNumber("1111");
         Assert.assertNull(result);
-
     }
+
+    @Test
+    public void testFindByName(){
+        phoneBook.add("Jack", "987");
+
+        String result = phoneBook.findByName("Jack");
+        Assert.assertEquals("987", result);
+
+        result = phoneBook.findByName("Vanya");
+        Assert.assertNull(result);
+    }
+
 }
