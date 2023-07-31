05ab22f291ed2c62c29b7d21cdb96fc65bd79772
serjzlo <serioga2003@yandex.ru>
2023-07-31 15:06:06 +0700

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -5,12 +5,16 @@ public class PhoneBook {
 
     static Map<String,String> contacts = new HashMap<>();
 
-    public static Integer add (String name, String number){
+    public Integer add (String name, String number){
         if(contacts.containsKey(name)){
             return -1;
         } else {
             contacts.put(name, number);
             return contacts.size();
         }
+    }
+
+    public String findByNumber(String number){
+        return null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -14,5 +14,19 @@ public class PhoneBookTest {
         Assert.assertEquals(2, result);
     }
 
+    @Test
+    public void testFindByNumber(){
+        phoneBook.add("John", "12345");
 
+        String result = phoneBook.findByNumber("12345");
+        Assert.assertEquals("John", result);
+
+        phoneBook.add("Petya", "321");
+        result = phoneBook.findByNumber("321");
+        Assert.assertEquals("Petya", result);
+
+        result = phoneBook.findByNumber("1111");
+        Assert.assertNull(result);
+
+    }
 }
