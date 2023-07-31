d0c9b010d3208b9892577b6d2721e7384aa47088
serjzlo <serioga2003@yandex.ru>
2023-07-31 15:07:01 +0700

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -3,10 +3,10 @@ import java.util.Map;
 
 public class PhoneBook {
 
-    static Map<String,String> contacts = new HashMap<>();
+    static Map<String, String> contacts = new HashMap<>();
 
-    public Integer add (String name, String number){
-        if(contacts.containsKey(name)){
+    public Integer add(String name, String number) {
+        if (contacts.containsKey(name)) {
             return -1;
         } else {
             contacts.put(name, number);
@@ -14,7 +14,12 @@ public class PhoneBook {
         }
     }
 
-    public String findByNumber(String number){
+    public String findByNumber(String number) {
+        for (Map.Entry<String, String> entry : contacts.entrySet()) {
+            if (entry.getValue().equals(number)){
+                return entry.getKey();
+            }
+        }
         return null;
     }
 }
